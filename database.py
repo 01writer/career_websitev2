12ccc859f2ebd96fae44dc04deac7935c5d814af
01writer <95424197+01writer@users.noreply.github.com>
2023-04-24 17:41:13 +0000

    add comment to project

--- database.py
+++ database.py
@@ -12,32 +12,44 @@ engine = create_engine(db_connection_cred,
 
 #executing SQL query to get data from planetscale database
 def get_data_from_db():
+  #opening connection with database
   with engine.connect() as con:
     result = con.execute(text('select * from jobs'))
+    #converting sqlalchemy object to list
     result_all = result.all()
+    #creating a empty list
     job_data_in_dict = []
+    #Iterating list of jobs
     for result in result_all:
+      #converting each item of list into dictionary and adding them to empty list
       job_data_in_dict.append(result._asdict())
   return job_data_in_dict
 
 
 def load_job(id):
+  #opening connection with databse
   with engine.connect() as conn:
+    #executing sql query to get job data of a specific id
     result = conn.execute(text('select * from jobs WHERE id = :ids'),
                           {"ids": id})
+    #converting sqlalchemy object to list
     row = result.all()
     if len(row) == 0:
       return None
     else:
+      #converting list to dictionary
       jobdis = row[0]._asdict()
       return jobdis
 
 
+#saving data to databse
 def insert_application_to_db(job_id, data):
+  #opening connecting to database
   with engine.connect() as conn:
     query = text(
       'insert into applications(job_id, full_name, email, linkedin_url, education, experience, resume_url) values(:job_id, :full_name, :email, :linkedin_url, :education, :experience, :resume_url)'
     )
+    #executing insert query and passing data fields
     conn.execute(
       query, {
         'job_id': job_id,
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+#importing Flask, render_template, jasonify and request classes from flask module
 from flask import Flask, render_template, jsonify, request
 from database import get_data_from_db, load_job, insert_application_to_db
 
+#Initializing Flask class object
 app = Flask(__name__)
 
 # JOBS = [{
@@ -21,33 +23,40 @@ app = Flask(__name__)
 # }]
 
 
+#decorating hello_world function with app
 @app.route("/")
+#hello_world funtion which get the data from database and pass that to rendered template
 def hello_world():
   JOBS = get_data_from_db()
+  #fetching job data and showing list of available job to home page
   return render_template('home.html', jobs=JOBS, company_name='Webbullz')
 
 
 @app.route('/api')
 def list_job():
+  #fetching data from database
   JOBS = get_data_from_db()
+  #returning data in json format
   return jsonify(JOBS)
 
 
+#funtion to get job details from database with the help of id 
 @app.route('/jobs/<id>')
 def get_job_description(id):
   job_disc = load_job(id)
-  print(type(job_disc))
-  print(job_disc)
-  # if not job_disc:
-  #   return "Not Found"
+  #showing details job description to user for which user click apply
   return render_template('job_page.html', job=job_disc)
 
-
+#function to collect data filled in form by post method and save that in database
 @app.route('/job/<id>/apply', methods=['post'])
 def get_applicant_details(id):
+  #get data filled in form and assign it into data
   data = request.form
+  #fetching job details by id
   job = load_job(id)
+  #saving form data into database
   insert_application_to_db(id, data)
+  #showing thank you page to user
   return render_template('thank_you.html', application=data, job=job)
 
 
